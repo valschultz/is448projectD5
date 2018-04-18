6c7ab66774e83c8c46b8377fbcf4761f96cc77ea
Andrew Peterson <andrewp2@umbc.edu>
2018-04-18 18:22:35 -0400

    fixed image and css links

---
+++ whos_in.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="EN">
+
+<head>
+    <title>View available machines | Collaborate</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="mockup.css" />
+</head>
+
+<body>
+<p><?php echo "hello" ?></p>
+</body>
