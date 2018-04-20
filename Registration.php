ca02c514d79bcf240b727a1f39361e890fd56d19
valschultz <schultz4@umbc.edu>
2018-04-20 10:16:13 -0400

    update formatting

--- Registration.php
+++ Registration.php
@@ -30,7 +30,6 @@
 		
 	if(preg_match("/^[A-z]{2}\d{5}\$/", $id))
 		{
-			echo "Valid name. <br />";
 			#This is where you check the database to ensure that the student id and password match to a row in the database
 			#This requires JavaScript onclic, but logic is here
 		$checkLogIn_query = "Select student_id, student_password, student_first_name, student_last_name FROM Student WHERE student_id = '$id' AND student_password = '$password'";
@@ -53,11 +52,10 @@
 				header("location: https://swe.umbc.edu/~mbrooks3/is448/project2/studenthomepage.php");
 			}
 
-   else{
-   	print("Incorrect id or password, try again!");
-	?>
+   else{?>
 			<div class = "center">
 				<p>
+					Incorrect id or password, try again! <br />
 					Click <a href="https://swe.umbc.edu/~schultz4/is448/projectModified/Registration.html"> HERE</a> to go back
 				</p>
 			</div>
@@ -67,10 +65,10 @@
  }
 	}
 		else{
-			echo "Invalid student id. Must be 2 letters followed by 5 numbers. <br />";
 			?>
 				<div class = "center">
 				<p>
+					Invalid student id. Must be 2 letters followed by 5 numbers. <br />
 					Click <a href="https://swe.umbc.edu/~schultz4/is448/projectModified/Registration.html"> HERE</a> to go back
 				</p>
 			</div>
