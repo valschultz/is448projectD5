f048257d21f376dff8af7845ddeb1b49cb98c9d3
shinny94 <shinny94@gmail.com>
2018-04-20 19:06:08 -0400

    Add files via upload

--- equipment_reservation.php
+++ equipment_reservation.php
@@ -13,9 +13,11 @@ if ($_SESSION['login_user']) {
 ?>
 	
 <head>
+	<title> Equipment Registration </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<!--Linking this page to the CSS style sheet: mockUp.css-->
+	
+	<!--Linking this page to the CSS style sheet: mockUp.css-->
 	<link rel="stylesheet" type="text/css" href="mockUp.css" />
 <head>	
 	
@@ -29,7 +31,7 @@ if ($_SESSION['login_user']) {
 
 
 
-
+<h1 align="center">Equipment Registration</h1>
 
 <div class = "menu">
 	<a class= "menu_link" href = "https://swe.umbc.edu/~mbrooks3/is448/project2/studenthomepage.php">
@@ -53,47 +55,44 @@ if ($_SESSION['login_user']) {
 	</a>
 </div>
 
-<div class= "statement" style="text-align:  center">
 
-	<h1>Equipment Registration </h1>
-	<h2>View the available machines below.</h2>
-	<p>
-		Please select which equipment you would like to use then which equipment # and what time you intend on using it:
+	<form action="submit_reservation.php" >
+		<p>
+		<h2> Which equipment would you like to reserve? </h2>
 	</p>
 
-	<form action="submit_reservation.php">
-		<select name="machine_id">
-			<option value="1" name="treadmill_1">Treadmill#1</option>
-			<option value="2" name="treadmill_2">Treadmill#2</option>
-			<option value="3" name="treadmill_3">Treadmill#3</option>
-			<option value="4" name="treadmill_4">Treadmill#4</option>
-			<option value="5" name="elliptical_1">Elliptical#1</option>
-			<option value="6" name="elliptical_2">Elliptical#2</option>
-			<option value="7" name="elliptical_3">Elliptical#3</option>
-			<option value="8" name="elliptical_4">Elliptical#4</option>
-			<option value="9" name="squatrack_1">Squat Rack#1</option>
-			<option value="10" name="squatrack_2">Squat Rack#2</option>
-			<option value="11" name="benchpress_1">Bench Press#1</option>
-			<option value="12" name="benchpress_2">Bench Press#2</option>
-		</select>
+<select name="machine_id">
+	<option value="1">Treadmill #1</option>
+	<option value="2">Treadmill #2</option>
+	<option value="3">Treadmill #3</option>
+	<option value="4">Treadmill #4</option>
+	<option value="5">Elliptical #1</option>
+	<option value="6">Elliptical #2</option>
+	<option value="7">Elliptical #3</option>
+	<option value="8">Elliptical #4</option>
+	<option value="9">Squat Rack #1</option>
+	<option value="10">Squat Rack #2</option>
+	<option value="11">Bench Press #1</option>
+	<option value="12">Bench Press #2</option>
+</select>
 
-		<h2>View the available times below.</h2>
-		<p>
-			Please select what time you intend on using it:
-		</p>
+	<p>
+		<h2> Please select the time to reserve the machine: </h2>
+	</p>
 
-		<select name="time_block" size="1">
-			<option value="7">7:00am</option>
-			<option value="8">8:00am</option>
-			<option value="9">9:00am</option>
-			<option value="10">10:00am</option>
-			<option value="11">11:00am</option>
-			<option value="12">12:00pm</option>
-		</select>
+<select name="time_block" size="1">
+	<option value="7">7:00 am</option>
+	<option value="8">8:00 am</option>
+	<option value="9">9:00 am</option>
+	<option value="10">10:00 am</option>
+	<option value="11">11:00 am</option>
+	<option value="12">12:00 pm</option>
+</select>
+
 		
-		<input type="hidden" value="anonymous" name="student_id">
+		<input type="hidden" value="anonymous" name="student_id"> </input>
 		
-		<button type="submit">Make a reservation</submit>
+		<button type="submit">Make a reservation</submit> </button>
 	</form>
 
 
