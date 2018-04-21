ac712ea4660a35bb3b701ab2d37aa559e46d19da
shinny94 <shinny94@gmail.com>
2018-04-20 23:11:39 -0400

    Add files via upload

--- submit_reservation.php
+++ submit_reservation.php
@@ -14,21 +14,16 @@ if ($_SESSION['login_user']) {
 </head>
 <body>
 <?php
-
 	$db = mysqli_connect("studentdb-maria.gl.umbc.edu","mbrooks3","mbrooks3","mbrooks3");
-
 	$time_block = mysqli_real_escape_string($db, $_GET['time_block']);
 	$machine_id = mysqli_real_escape_string($db, $_GET['machine_id']);
 	$student_id = mysqli_real_escape_string($db, $_GET['student_id']);
-	$student_id = htmlspecialchars("yx68259");
-
 
 	$result = mysqli_query($db,
 		"SELECT * FROM Schedule WHERE time_block='$time_block' AND 
 			machine_id= '$machine_id'");
 	$num_rows = mysqli_num_rows($result);
 	$already_reserved = $num_rows > 0;
-
 	// instead of directing to another html page will use alert messages from Javascript in the next part 
 	if ($already_reserved) {
 		header('Location: error.html');
@@ -38,7 +33,7 @@ if ($_SESSION['login_user']) {
 		mysqli_query($db,"INSERT INTO Schedule (time_block, student_id, machine_id) 
 			VALUES ('$time_block', NULL, '$machine_id')");
 		mysqli_query($db,"INSERT INTO Student_Machines (student_id, machine_id)
-			VALUES('$student_id', '$machine_id')"); 
+			VALUES(NULL, '$machine_id')"); 
 	// $results = mysqli_query($db, 'select * from Schedule JOIN Machines on Schedule.machine_id = Machines.machine_id');
 	header('Location: confirmation.html');
 	die();
